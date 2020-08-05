218ac247805cbae0b430068a6b4062308c31f3bf
8bitjustin <j.olson.digital@gmail.com>
2020-08-05 09:42:02 -0700

    Completed posts function, posts now post correctly. Also changed name of textarea on index.html to 'content'.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -17,42 +17,43 @@ class BlogPost(db.Model):
         return 'Blog post ' + str(self.id)
 
 
-all_posts = [
-    {
-        'title': 'Post 1',
-        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-                   ' Donec leo lacus, pretium at turpis eget, lacinia'
-                   ' porttitor tellus. Pellentesque ipsum mauris, pretium quis'
-                   ' posuere sit amet, gravida eget augue. Orci varius natoque'
-                   ' penatibus et magnis dis parturient montes, nascetur'
-                   ' ridiculus mus.',
-        'created': 'Jan 1, 2020 @ 8:00am'
-    },
-    {
-        'title': 'Post 2',
-        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-                   ' Donec leo lacus, pretium at turpis eget, lacinia'
-                   ' porttitor tellus. Pellentesque ipsum mauris, pretium quis'
-                   ' posuere sit amet, gravida eget augue. Orci varius natoque'
-                   ' penatibus et magnis dis parturient montes, nascetur'
-                   ' ridiculus mus.',
-        'created': 'Jan 2, 2020 @ 8:00am'
-    },
-    {
-        'title': 'Post 3',
-        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-                   ' Donec leo lacus, pretium at turpis eget, lacinia'
-                   ' porttitor tellus. Pellentesque ipsum mauris, pretium quis'
-                   ' posuere sit amet, gravida eget augue. Orci varius natoque'
-                   ' penatibus et magnis dis parturient montes, nascetur'
-                   ' ridiculus mus.',
-        'created': 'Jan 3, 2020 @ 8:00am'
-    }
-]
+# all_posts = [
+#     {
+#         'title': 'Post 1',
+#         'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+#                    ' Donec leo lacus, pretium at turpis eget, lacinia'
+#                    ' porttitor tellus. Pellentesque ipsum mauris, pretium quis'
+#                    ' posuere sit amet, gravida eget augue. Orci varius natoque'
+#                    ' penatibus et magnis dis parturient montes, nascetur'
+#                    ' ridiculus mus.',
+#         'created': 'Jan 1, 2020 @ 8:00am'
+#     },
+#     {
+#         'title': 'Post 2',
+#         'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+#                    ' Donec leo lacus, pretium at turpis eget, lacinia'
+#                    ' porttitor tellus. Pellentesque ipsum mauris, pretium quis'
+#                    ' posuere sit amet, gravida eget augue. Orci varius natoque'
+#                    ' penatibus et magnis dis parturient montes, nascetur'
+#                    ' ridiculus mus.',
+#         'created': 'Jan 2, 2020 @ 8:00am'
+#     },
+#     {
+#         'title': 'Post 3',
+#         'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+#                    ' Donec leo lacus, pretium at turpis eget, lacinia'
+#                    ' porttitor tellus. Pellentesque ipsum mauris, pretium quis'
+#                    ' posuere sit amet, gravida eget augue. Orci varius natoque'
+#                    ' penatibus et magnis dis parturient montes, nascetur'
+#                    ' ridiculus mus.',
+#         'created': 'Jan 3, 2020 @ 8:00am'
+#     }
+# ]
 
 
 @app.route('/')
 def index():
+    all_posts = BlogPost.query.order_by(BlogPost.created).all()
     return render_template('index.html', posts=all_posts)
 
 
@@ -63,7 +64,16 @@ def login():
 
 @app.route('/posts', methods=['GET', 'POST'])
 def posts():
-    return render_template('posts.html', posts=all_posts)
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_content = request.form['content']
+        new_post = BlogPost(title=post_title, content=post_content)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        all_posts = BlogPost.query.order_by(BlogPost.created).all()
+        return render_template('posts.html', posts=all_posts)
 
 
 @app.route('/edit')
