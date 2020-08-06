2e3c2ba6304ff0e7d0f98e56bd34028722b66650
8bitjustin <j.olson.digital@gmail.com>
2020-08-05 21:18:10 -0700

    Added second db, users, and created super basic User class with bind key.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_BINDS'] = {'users': 'sqlite:///users.db'}
 db = SQLAlchemy(app)
 
 
@@ -17,13 +18,17 @@ class BlogPost(db.Model):
         return 'Blog post ' + str(self.id)
 
 
+class User(db.Model):
+    __bind_key__ = 'users'
+
+
 @app.route('/')
 def index():
     all_posts = BlogPost.query.order_by(BlogPost.created).all()
     return render_template('index.html', posts=all_posts)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
