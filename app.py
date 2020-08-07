1fa61047ec59666940017c21f94a95ad1e991104
8bitjustin <j.olson.digital@gmail.com>
2020-08-07 15:41:06 -0700

    Testing, changed back to datetime.now.

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now)
 
     def __repr__(self):
         return 'Blog post ' + str(self.id)
