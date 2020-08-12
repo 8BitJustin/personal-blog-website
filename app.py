f02d461e9edda085c04a13f9a5f70cb70ddb04e6
8bitjustin <j.olson.digital@gmail.com>
2020-08-12 11:50:05 -0700

    Added app.py.

---
+++ app.py
@@ -0,0 +1,112 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required,\
+    logout_user
+from datetime import datetime
+
+app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = \
+#     'postgresql://postgres:C0d!ng01@localhost/jod_blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ebfscaqqswkhez:21d04ace' \
+                                        '5173378de0930f459f90392664aaafcc69' \
+                                        '40c83f783202363f41c5f4@ec2-52-204-' \
+                                        '20-42.compute-1.amazonaws.com:5432' \
+                                        '/ddu68mi09svum?sslmode=require'
+app.config['SQLALCHEMY_BINDS'] = {'users': 'sqlite:///users.db'}
+app.config['SECRET_KEY'] = 'secretkeydundundunn'
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class BlogPost(db.Model):
+    __tablename__="data"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now)
+
+    def __repr__(self):
+        return 'Blog post ' + str(self.id)
+
+
+class User(UserMixin, db.Model):
+    __bind_key__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(30), unique=True)
+    password = db.Column(db.String(10))
+
+# To create a user, within Python, type the following command:
+# from app import db, User
+# db.session.add(User(email = 'email address', password = 'password'))
+# db.session.commit()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route('/')
+def index():
+    all_posts = BlogPost.query.order_by(BlogPost.created.desc()).all()
+    return render_template('index.html', posts=all_posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user_input = request.form['email']
+        user_pw = request.form['password']
+        user = User.query.all()[0]
+        if user_input == user.email and user_pw == user.password:
+            login_user(user)
+            return redirect(url_for('posts'))
+        return redirect(url_for('login'))
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/posts', methods=['GET', 'POST'])
+@login_required
+def posts():
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_content = request.form['content']
+        new_post = BlogPost(title=post_title, content=post_content)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        all_posts = BlogPost.query.order_by(BlogPost.created.desc()).all()
+        return render_template('posts.html', posts=all_posts)
+
+
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+    post = BlogPost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts')
+
+
+@app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    post = BlogPost.query.get_or_404(id)
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.content = request.form['content']
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('edit.html', post=post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
